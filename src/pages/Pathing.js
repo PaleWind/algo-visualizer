8746cb5c4bc57a3ea368bf77bb24d53f8c26dc98
paul wandrei <paulwandrei@pauls-MacBook-Pro.local>
2023-01-22 17:55:36 -0500

    displaying graph

--- src/pages/Pathing.js
+++ src/pages/Pathing.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 
 const Pathing = () => {
+  const [graph, setGraph] = useState([]);
+
+  useEffect(() => {
+    setGraph(() => generateGraph());
+  }, []);
+
+  function generateGraph() {
+    let graph = [];
+    for (let i = 0; i < 15; i++) {
+      let row = [];
+      for (let j = 0; j < 15; j++) {
+        row.push([]);
+      }
+      graph.push(row);
+    }
+    console.log(graph);
+    return graph;
+  }
+
   return (
     <>
       <div className="controls-container"></div>
+
       <div className="info-container"></div>
-      <div className="visualizer-container"></div>
+
+      <div className="pathing-container">
+        <div className="path-graph">
+          {graph.map((row, rowi) => {
+            return (
+              <div>
+                {row.map((col, coli) => {
+                  return <div className="graph-node" key={coli}></div>;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
